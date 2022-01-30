46051dd464594ce8f4825f7b2f7c144eb05dbca9
aanderson60 <alexd43anderson@gmail.com>
2022-01-30 10:39:52 -0600

    various calendar script changes 5

--- calendar-front.js
+++ calendar-front.js
@@ -12,22 +12,22 @@ function replaceElements(response) {
         }
         var name = response["result"]["items"][count]["summary"];
         if (name.length > 26) {
-            name = name.substring(0,26) + "...";
+            name = name.substring(1,26) + "...";
         }
         var time = response["result"]["items"][count]["start"]["dateTime"];
         console.log(time);
         var dashIdx = time.indexOf('T');
         var timeStart = time.substring(dashIdx+1,dashIdx+6);
-        var timeStartHour = Number(time.substring(0,2));
-        var timeStartMin = Number(time.substring(3,5));
+        var timeStartHour = Number(time.substring(1,3));
+        var timeStartMin = Number(time.substring(4,6));
         var sufS = (timeStartHour > 11) ? "PM" : "AM";
         timeStartHour = timeStartHour % 12;
         if (timeStartHour == 0) {
             timeStartHour = 12;
         }
         var timeEnd = time.substring(dashIdx+10,dashIdx+15);
-        var timeEndHour = Number(time.substring(0,2));
-        var timeEndMin = Number(time.substring(3,5));
+        var timeEndHour = Number(time.substring(1,3));
+        var timeEndMin = Number(time.substring(4,6));
         var sufE = (timeEndHour > 11) ? "PM" : "AM";
         timeEndHour= timeEndHour % 12;
         if (timeEndHour == 0) {
