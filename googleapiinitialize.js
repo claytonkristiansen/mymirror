150a37fe60a41215ffff6a669cda00ccc79d5495
claytonkristiansen <clayton.kristiansen@hotmail.com>
2022-01-29 20:50:10 -0600

    test3

--- googleapiinitialize.js
+++ googleapiinitialize.js
@@ -2,7 +2,7 @@
 var CLIENT_ID = '729667979950-8u5r6kkclsj5cisldvfbj51gfh07mnjn.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyCLCaBMOprD4ChtjoMjBRUYnacuvY4HcXc';
 
-// Array of API discovery doc URLs for APIs used by the quickstart
+// Array of API discovery doc URLs for APIs used     by the quickstart
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
 // Authorization scopes required by the API; multiple scopes can be
