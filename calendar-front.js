538411475dd7ae9ac2e6816de8d105dd02dd4d3a
aanderson60 <alexd43anderson@gmail.com>
2022-01-30 02:10:57 -0600

    Calendar interface 2

---
+++ calendar-front.js
@@ -0,0 +1,11 @@
+function replaceElements(response) {
+    var numEntries = response["result"]["items"].length;
+    var count = 0;
+    while (numEntries > 0) {
+        var name = response["result"]["items"][count]["summary"];
+        var time = response["result"]["items"][count]["summary"]["start"]["dateTime"];
+        document.getElementsByClassName("event-entry")[count].innerHTML = "<h2>name</h2><p>time</p>"
+        numEntries--;
+        count++;
+    }
+}
