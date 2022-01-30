635736c6dd958f22a11d9fff9a81aabfd986d7d6
claytonkristiansen <clayton.kristiansen@hotmail.com>
2022-01-29 21:11:01 -0600

    test5

--- googleapiinitialize.js
+++ googleapiinitialize.js
@@ -16,8 +16,34 @@ var signoutButton = document.getElementById('signout_button');
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-  console.log("Authenticated fine");
+  // 1. Load the JavaScript client library.
+  gapi.load('client', init);
+
+  async function init() {
+    // 2. Initialize the JavaScript client library.
+    await gapi.client.init({
+      discoveryDocs: ['https://discovery.googleapis.com/$discovery/rest']
+    });
+    start();
+  }
+
+  const API_QUERY = 'android';
+  async function start() {
+    try {
+      // 3. Make the API request.
+      const apiRequest = await gapi.client.discovery.apis.list();
+      const result = JSON.parse(apiRequest.body);
+      
+      // 4. Log the results of the API request
+      const androidAPIs = result.items.filter(api => api.id.startsWith(API_QUERY));
+      const androidAPITitles = androidAPIs.map(api => api.title);
+      const uniqueAndroidAPITitles = [...new Set(androidAPITitles).values()];
+      document.getElementById('result').innerHTML = 
+        `${uniqueAndroidAPITitles.length} APIs:<br>${uniqueAndroidAPITitles.join('<br/> ')}`;
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
 
 /**
