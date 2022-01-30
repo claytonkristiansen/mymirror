fbb7143d9097b524aee6ee25edbe81bde4b358f3
aanderson60 <alexd43anderson@gmail.com>
2022-01-30 11:37:11 -0600

    Added comments

--- calendar-front.js
+++ calendar-front.js
@@ -1,30 +1,41 @@
+// This function receives the Google Calendar JSON and injects the data into the frontend
 function replaceElements(response) {
+    // Get number of entries present in JSON
     var numEntries = response["result"]["items"].length;
     var count = 0;
     var numDel = 0;
+    // See how many entries need to be ignored (capped at 4 events)
     if (numEntries < 4) {
         numDel = 4 - numEntries;
     }
-                                                  
+    // Iterate through each entry                                      
     while (numEntries > 0) {
+        // If we pass the 4th event, break
         if (count > 3) {
             break;
         }
+        // Get name from JSON
         var name = response["result"]["items"][count]["summary"];
+        // Clip off end if too long
         if (name.length > 26) {
             name = name.substring(0,26) + "...";
         }
+        // Get start and end times
         var time = response["result"]["items"][count]["start"]["dateTime"];
         var timeEndT = response["result"]["items"][count]["end"]["dateTime"];
         console.log(time);
+        // Various string manip to get hour and minute
         var dashIdx = time.indexOf('T');
         var dashIdx2 = timeEndT.indexOf('T');
         var timeStart = time.substring(dashIdx+1,dashIdx+6);
         var timeStartHour = Number(timeStart.substring(0,2));
         var timeStartMin = Number(timeStart.substring(3,5));
         timeStartMin = timeStartMin.toString();
+        // Add zero to minute if it is 1 digit
         timeStartMin = (timeStartMin.length != 1) ? timeStartMin : "0" + timeStartMin;
+        // Get AM/PM suffix
         var sufS = (timeStartHour > 11) ? "PM" : "AM";
+        // Convert 24-hour time to 12-hour time
         timeStartHour = timeStartHour % 12;
         if (timeStartHour == 0) {
             timeStartHour = 12;
@@ -40,8 +51,10 @@ function replaceElements(response) {
             timeEndHour = 12;
         }
         console.log(count + ": StartTime:"+timeStart+" EndTime:"+timeEnd)
+        // Formatted time
         var timeF = timeStartHour.toString() + ":" + timeStartMin.toString() + sufS;
         
+        // Inject name/time into entry
         document.getElementsByClassName("event-entry")[count].innerHTML = "<h1>"+name+"</h1><p>"+timeF+"</p>";
         numEntries--;
         count++;
@@ -49,6 +62,7 @@ function replaceElements(response) {
 
 
     while (numDel > 0) {
+        // If there are less than 4 entries remove the div for leftover items
         document.getElementsByClassName("event-entry")[count].style.display = 'none';
         count++;
         numDel--;
--- time.js
+++ time.js
@@ -1,4 +1,6 @@
+// Function retrieves the current date/time and injects it into the fronend
 var x = setInterval(function() {
+    // Getting current date/time
     var date = new Date();
     var year = date.getFullYear();
     const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -6,19 +8,23 @@ var x = setInterval(function() {
     var month = MONTHS[date.getMonth()];
     var day = DAYS[date.getDay()];
     var dayNum = date.getDate();
+    // Insert various days and times
     document.getElementById("month-year").innerHTML = month + " " + year;
     document.getElementById("day").innerHTML = day;
     document.getElementById("day-num").innerHTML = dayNum;
     console.log(day+dayNum+month+year);
 
     var hour = new Date().getHours();
+    // Get AM/PM suffix
     var suf = (hour > 11) ? " PM" : " AM";
+    // Convert to 12 hour time
     hour = hour % 12;
     if (hour == 0) {
         hour = 12;
     }
     var min = new Date().getMinutes();
     min = min.toString();
+    // Add extra zero if minutes has only 1 digit
     min = (min.length != 1) ? min : "0" + min;
     var time = hour + ":" + min;
     document.getElementById("time").innerHTML = time + suf;
